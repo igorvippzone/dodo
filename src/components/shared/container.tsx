49d73bf35b5d9e2a2d2dddbb350d847d61c8813e
Igor Agalarov <igorvippzone@gmail.com>
2025-10-14 22:30:47 +0300

    Начало

---
+++ src/components/shared/container.tsx
@@ -0,0 +1,12 @@
+import { cn } from '@/lib/utils'
+import React from 'react'
+
+type Props = {
+	className?: string
+}
+
+export const Container: React.FC<React.PropsWithChildren<Props>> = ({ className, children }) => {
+	return (
+		<div className={cn('mx-auto max-w-[1200px]', className)}>{children}</div>
+	)
+}
